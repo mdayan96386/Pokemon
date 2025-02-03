ab082ac5449369b60bd300d13a65754715d98b02
ayan96386 <ayan96386@gmail.com>
2025-02-03 13:10:13 +0530

    Remove Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
 import Form from "./Components/Form";
 import Card from "./Components/Card";
 import { PokeProvider } from "./Context/PokeContext";
@@ -7,7 +6,6 @@ import { PokeProvider } from "./Context/PokeContext";
 function App() {
   return (
     <PokeProvider>
-      <Navbar />
       <div className=" p-5  2xl:p-5 2xl:flex items-center justify-center flex-col">
         <Form />
         <Card />
